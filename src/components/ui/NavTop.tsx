63c99b29f8f4dcfc2eb57be09069762d450ec6f7
Shahriar Rahman Anik <anikshariarrahman@gmail.com>
2025-02-02 01:09:25 +0600

    nav top

--- src/components/ui/NavTop.tsx
+++ src/components/ui/NavTop.tsx
@@ -1,10 +1,15 @@
 
 export default function NavTop() {
-  return (
-    <div>
-        <div>
-            
+    return (
+        <div className="container">
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                    <h3 style={{ marginTop: "6px", marginBottom: "6px" }}>Get 10% off on your first order</h3>
+                </div>
+                <div>
+                    <h3 style={{ marginTop: "6px", marginBottom: "6px" }}>Phone: 0123456789</h3>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }

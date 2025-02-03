d950d129ba5c3c44ed2e2b671da602fa400dca4a
Shahriar Rahman Anik <anikshariarrahman@gmail.com>
2025-02-04 01:15:15 +0600

    cart added

--- src/components/home/NewArrival.tsx
+++ src/components/home/NewArrival.tsx
@@ -1,12 +1,14 @@
+import Card from "../ui/Card";
 import SectionHeader from "../ui/SestionHeader";
+import test from '../../assets/banner/pen-notebook-near-stationery.jpg';
 
 
 export default function NewArrival() {
   return (
-    <div style={{ marginTop: "50px" }} className="container ">
+    <div style={{ marginTop: "50px" , marginBottom: "50px" }} className="container ">
       <SectionHeader title="New Arrival" />
       <div>
-        
+       <Card image={test} title="Note book" price="$10" size="Size "/> 
       </div>
     </div>
   )
---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,49 @@
+import '../../styles/Card.css';
+
+export default function Card({ image, title, price, size }: any) {
+  return (
+    <div>
+      <div className="card">
+        <div className="image_container">
+          <img src={image} alt="" />
+        </div>
+        <div className="title">
+          <span>{title}</span>
+        </div>
+        {/* <div className="size ">
+          <span>{size}</span>
+          <ul className="list-size ">
+            <li className="item-list bg-primary"><button className="item-list-button ">37</button></li>
+            <li className="item-list bg-primary"><button className="item-list-button">38</button></li>
+            <li className="item-list"><button className="item-list-button">39</button></li>
+            <li className="item-list"><button className="item-list-button">40</button></li>
+            <li className="item-list"><button className="item-list-button">41</button></li>
+          </ul>
+        </div> */}
+        <div className="action">
+          <div className="price">
+            <span>{price}</span>
+          </div>
+          <button className="cart-button bg-primary">
+            <svg
+              className="cart-icon"
+              stroke="currentColor"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+              ></path>
+            </svg>
+            <span>Add to cart</span>
+          </button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
